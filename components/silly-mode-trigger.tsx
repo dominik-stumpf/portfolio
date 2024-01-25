95f7981f6afa9e838bd8e56c681c72a6f38450ac
Dominik Stumpf <s.dominik.work@gmail.com>
2024-01-25 15:49:29 +0100

    feat: add flash text effect

--- components/silly-mode-trigger.tsx
+++ components/silly-mode-trigger.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import { useTextExpressionModifier } from '@/hooks/use-text-expression-modifier';
-import { WavyText } from './text-effects';
+import { FlashText, WavyText } from './text-effects';
 
 export function SillyModeTrigger() {
   const { setActive } = useTextExpressionModifier();
 
   return (
-    <button className="" onClick={() => setActive(true)} type="button">
-      <WavyText>silly things</WavyText>
-    </button>
+    <FlashText>
+      <button className="" onClick={() => setActive(true)} type="button">
+        <WavyText>silly things</WavyText>
+      </button>
+    </FlashText>
   );
 }
--- components/text-effects.tsx
+++ components/text-effects.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import '@/styles/animations.css';
+import type { ReactNode, MouseEvent, KeyboardEvent } from 'react';
 
 export function WavyText({ children }: { children: string }) {
   return (
@@ -14,6 +17,30 @@ export function WavyText({ children }: { children: string }) {
           {char}
         </span>
       ))}
+    </span>
+  );
+}
+
+export function FlashText({ children }: { children: ReactNode }) {
+  function flash(event: MouseEvent | KeyboardEvent) {
+    event.currentTarget.animate(
+      {
+        color: [`hsl(${Math.random() * 360}deg 90% 60%)`, 'inherit'],
+        transform: ['scale(1.05)', 'scale(1)'],
+      },
+      {
+        duration: 512,
+        iterations: 1,
+      },
+    );
+  }
+  return (
+    <span
+      onClick={flash}
+      onKeyDown={flash}
+      className="inline-block whitespace-pre"
+    >
+      {children}
     </span>
   );
 }
