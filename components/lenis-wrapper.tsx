fc0ff368971e00ec96437fc014ece58caa597310
Dominik Stumpf <s.dominik.work@gmail.com>
2024-03-14 09:56:03 +0100

    chore: migrate to svelte

--- components/lenis-wrapper.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { ReactLenis } from '@studio-freight/react-lenis';
-import type { ReactNode } from 'react';
-
-export function LenisWrapper({ children }: { children: ReactNode }) {
-  return <ReactLenis root>{children}</ReactLenis>;
-}
