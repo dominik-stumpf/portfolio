23e92050fa4af76f8e6824438c33ce3b7ddf95e6
Dominik Stumpf <s.dominik.work@gmail.com>
2024-03-18 16:24:09 +0100

    refactor: move retrieve-formatter.ts to utils folder

--- src/routes/api/weblogs/+server.ts
+++ src/routes/api/weblogs/+server.ts
@@ -1,5 +1,5 @@
 import { json } from '@sveltejs/kit';
-import { retrieveFrontmatter } from 'src/lib/unified-utils/retrieve-frontmatter';
+import { retrieveFrontmatter } from '$lib/utils/retrieve-frontmatter';
 import { routes } from 'src/site-config/site-data';
 
 async function fetchMarkdownWeblogs() {
