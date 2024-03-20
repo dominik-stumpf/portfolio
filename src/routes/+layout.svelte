4e4b7844ec2b4f99e86b00ac1753d320a78558a7
Dominik Stumpf <s.dominik.work@gmail.com>
2024-03-20 11:17:40 +0100

    chore: add head meta tags for site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,12 +7,44 @@ import { siteData } from 'src/site-config/site-data';
 </script>
 
 <svelte:head>
+  <meta charset="utf-8" />
+  <link rel="icon" href="/favicon.svg" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link
     rel="alternate"
     type="application/rss+xml"
     title={`RSS feed for ${siteData.link}`}
     href="/api/feed"
   />
+  <meta charset="utf-8" /><meta
+    name="viewport"
+    content="width=device-width, initial-scale=1"
+  />
+  <meta
+    name="google-site-verification"
+    content="VSU4j-HwrTFTdEKECQOMKDBmWSdo9nQmUZWjdbEn_Uo"
+  />
+  <meta
+    name="theme-color"
+    media="(prefers-color-scheme: dark)"
+    content="#000000"
+  />
+  <meta
+    name="theme-color"
+    media="(prefers-color-scheme: light)"
+    content="#ffffff"
+  />
+  <meta name="color-scheme" content="dark light" />
+  <title>{siteData.title}</title>
+  <meta name="description" content={siteData.description} />
+  <meta name="application-name" content={siteData.title} />
+  <link rel="author" href={siteData.link} />
+  <meta name="author" content={siteData.maintainerName} />
+  <meta name="keywords" content="resume,weblogs,programming" />
+  <meta name="creator" content={siteData.maintainerName} />
+  <!-- <link rel="manifest" href="/manifest.webmanifest" /> -->
+  <!-- <link rel="shortcut icon" href="/images/icons/icon-72x72.png" /> -->
+  <!-- <link rel="apple-touch-icon" href="/images/icons/icon-128x128.png" /> -->
 </svelte:head>
 
 <div
