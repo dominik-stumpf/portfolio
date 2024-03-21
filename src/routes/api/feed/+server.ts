777f7b61b9df070a6c6d242aeec7085b0bf9f93c
Dominik Stumpf <s.dominik.work@gmail.com>
2024-03-21 18:16:00 +0100

    feat: extend the rss feed with additional metadata

--- src/routes/api/feed/+server.ts
+++ src/routes/api/feed/+server.ts
@@ -4,9 +4,19 @@ import { siteData } from 'src/site-config/site-data';
 
 const { title, description, link, maintainerEmail, maintainerName } = siteData;
 
+function parseKeywords(keywords: string[] | undefined): string {
+  if (keywords === undefined) {
+    return '';
+  }
+  return ` Keywords: ${keywords.join(', ')}`;
+}
+
 function generateRssFeedFrom(weblogs: Weblogs) {
   const siteUrl = new URL(link);
   const maintainer = `${maintainerEmail} (${maintainerName})`;
+  const sortedWeblogs = weblogs.sort((a, b) => {
+    return b.metadata.publishedAt.valueOf() - a.metadata.publishedAt.valueOf();
+  });
 
   return `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
@@ -18,8 +28,9 @@ function generateRssFeedFrom(weblogs: Weblogs) {
 <managingEditor>${maintainer}</managingEditor>
 <webMaster>${maintainer}</webMaster>
 <atom:link href="${new URL('api/feed', siteUrl)}" rel="self" type="application/rss+xml"/>
-${weblogs
+${sortedWeblogs
   .filter((weblog) => weblog.metadata.language.startsWith('en'))
+  .slice(0, 8)
   .map((weblog) => {
     const postUrl = new URL(weblog.path, siteUrl);
     return `<item>
@@ -27,7 +38,9 @@ ${weblogs
 <link>${postUrl}</link>
 <author>${maintainer}</author>
 <title>${weblog.metadata.title}</title>
-<description>${weblog.metadata.lead}</description>
+<description>${weblog.metadata.lead} ${
+      weblog.readTimeResults.text
+    }.${parseKeywords(weblog.metadata.keywords)}</description>
 <pubDate>${weblog.metadata.publishedAt.toUTCString()}</pubDate>
 </item>`;
   })
