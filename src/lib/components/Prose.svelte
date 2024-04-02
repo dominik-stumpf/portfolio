c85ab4caf4fcdc209b9117583d69b0f87b759bb5
Dominik Stumpf <s.dominik.work@gmail.com>
2024-04-02 09:52:39 +0200

    fix: make table 100% width

--- src/lib/components/Prose.svelte
+++ src/lib/components/Prose.svelte
@@ -1,6 +1,6 @@
 <article
   class="prose prose-custom w-full break-words dark:prose-invert lg:prose-xl
-  prose-pre:p-0 prose-table:block prose-table:overflow-auto
+  prose-pre:p-0 prose-table:overflow-auto
   lg:prose-pre:text-base print:prose-sm max-w-none"
 >
   <slot />
